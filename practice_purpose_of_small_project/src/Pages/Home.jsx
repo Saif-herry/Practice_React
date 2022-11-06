b894103c52d102b6b5cf9baac2427d5d9dbece1d
Mohammad Saif <saifsami321md@gmail.com>
2022-11-06 15:33:48 +0530

    addition practice done

--- practice_purpose_of_small_project/src/Pages/Home.jsx
+++ practice_purpose_of_small_project/src/Pages/Home.jsx
@@ -1,52 +1,34 @@
-
-import React from "react"
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-    const [data,setData] = useState(0)
-    const [data1,setData1] = useState(0)
-    const [adddata,setAdddata] = useState(0)
-    // const [subsdata,setsubsdata] = useState(0)
-    
+const [frstnum,setFrstNum] = useState(0)
+const [secondnum,setSecondNum] = useState(0)
+const [adddata,setAdddata] = useState(0)
 
+const handleChange = (e) => {
+let val = Number(e.target.value);
+console.log(val)
+setFrstNum(val)
+}
 
+const handleChange1 = (e) => {
+let val1 = Number(e.target.value);
+setSecondNum(val1)
+}
 
-    function handleChange(e){
-        let val = Number(e.target.value);
-        setData(val)
-    }
-    
-    function handleChange1(e){
-          let val1 = Number(e.target.value);
-          setData1(val1)
-    }
-    const handleSubmit = () =>{ 
-        // let data2= Number(data)
-        // let data3 = Number(data1)
-    setAdddata(data+data1)   
-    }
-
-    
-
-
-    <div>
-           
-    </div>
+const handleClick = () => {
+    setAdddata(frstnum+secondnum)
+}
     return(
         <div>
-             <h1>{adddata}</h1>
-            <div className="formdiv">
-          {/* <input type="" placeholder="Enter Image url"/> */}
-          <input type="number" placeholder="Enter First Number" onChange={handleChange}/>
-          <input type="number"  placeholder="Enter Second Number" onChange={handleChange1}/>
-          <div>
-          <button onClick={handleSubmit}>Add</button>
-          {/* <button onClick={handleSubmit1}>Substract</button> */}
-          
-          </div>
-          </div>
+            <h1>{adddata}</h1>
+         <div className="formInput">
+            <input type="number" placeholder="Enter First Name" onChange={handleChange}/>
+            <input type="number" placeholder="Enter Second Number" onChange={handleChange1}/>
+            <button onClick={handleClick}>Add</button>
+         </div>   
         </div>
     )
 }
 
-export default Home
+export default Home;
