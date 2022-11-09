38c5820793d27f473c010755ebed8f6fc6bfce8e
Mohammad Saif <saifsami321md@gmail.com>
2022-11-09 11:51:06 +0530

    calculator practice

--- practice_purpose_of_small_project/src/App.js
+++ practice_purpose_of_small_project/src/App.js
@@ -1,47 +1,44 @@
+import React, { useState } from "react";
+import "./App.css"
 
-import { useState } from 'react';
-import './App.css';
-import Signup from './Pages/Signup';
+const App = () =>{
+const [result,setResult] = useState("")
 
-
-function App() {
-  const [result,setResult] = useState("")
-
-  const handleClick = (e) => {
-        setResult(result.concat(e.target.value))
-  }
-
-  const handleClear = () => {
-    setResult("")
-  }
-
-  const calculate = () => {
-    setResult(eval(result).toString())
-  }
-  return (
-    <div className="App">
-     {/* <Signup/> */}
-     <input type="text" placeholder='0' value={result} id="answer" className='button'/>
-     <input type="button" value="9" className='button' onClick={handleClick}/>
-     <input type="button" value="8" className='button' onClick={handleClick}/>
-     <input type="button" value="7" className='button' onClick={handleClick}/>
-     <input type="button" value="6" className='button' onClick={handleClick}/>
-     <input type="button" value="5" className='button' onClick={handleClick}/>
-     <input type="button" value="4" className='button' onClick={handleClick}/>
-     <input type="button" value="3" className='button' onClick={handleClick}/>
-     <input type="button" value="2" className='button' onClick={handleClick}/>
-     <input type="button" value="1" className='button' onClick={handleClick}/>
-     <input type="button" value="0" className='button' onClick={handleClick}/>
-     <input type="button" value="+" className='button' onClick={handleClick}/>
-     <input type="button" value="-" className='button' onClick={handleClick}/>
-     <input type="button" value="*" className='button' onClick={handleClick}/>
-     <input type="button" value="/" className='button' onClick={handleClick}/>
-     <input type="button" value="%" className='button' onClick={handleClick}/>
-     <input type="button" value="=" className='button button1' onClick={calculate}/>
-     <input type="button" value="Clear" className='button button1' onClick={handleClear}/>
-
-    </div>
-  );
+const handleClick = (e) => {
+  setResult(result.concat(e.target.value))
 }
 
-export default App;
+const clearfunction = () => {
+  setResult("")
+}
+
+const calculate = () => {
+  setResult(eval(result).toString())
+}
+  return(
+    <div className="App">
+      <h1>Hello</h1>
+      <input type="text" value={result} placeholder="0"/>
+      <input type="button" value="9" onClick={handleClick}/>
+      <input type="button" value="8" onClick={handleClick}/>
+      <input type="button" value="7" onClick={handleClick}/>
+      <input type="button" value="6" onClick={handleClick}/>
+      <input type="button" value="5" onClick={handleClick}/>
+      <input type="button" value="4" onClick={handleClick}/>
+      <input type="button" value="3" onClick={handleClick}/>
+      <input type="button" value="2" onClick={handleClick}/>
+      <input type="button" value="1" onClick={handleClick}/>
+      <input type="button" value="0" onClick={handleClick}/>
+      <input type="button" value="+" onClick={handleClick}/>
+      <input type="button" value="-" onClick={handleClick}/>
+      <input type="button" value="*" onClick={handleClick}/>
+      <input type="button" value="/" onClick={handleClick}/>
+      <input type="button" value="%" onClick={handleClick}/>
+      <input type="button" value="Clear" onClick={clearfunction}/>
+      <input type="button" value="=" onClick={calculate}/>
+    </div>
+  )
+}
+
+
+export default App
