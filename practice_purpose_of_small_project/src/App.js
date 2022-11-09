dcb893b2beb7d777d996689c842da8cc98cc7e25
Mohammad Saif <saifsami321md@gmail.com>
2022-11-09 11:31:15 +0530

    calculate practice has been done

--- practice_purpose_of_small_project/src/App.js
+++ practice_purpose_of_small_project/src/App.js
@@ -1,12 +1,45 @@
 
+import { useState } from 'react';
 import './App.css';
 import Signup from './Pages/Signup';
 
 
 function App() {
+  const [result,setResult] = useState("")
+
+  const handleClick = (e) => {
+        setResult(result.concat(e.target.value))
+  }
+
+  const handleClear = () => {
+    setResult("")
+  }
+
+  const calculate = () => {
+    setResult(eval(result).toString())
+  }
   return (
     <div className="App">
-     <Signup/>
+     {/* <Signup/> */}
+     <input type="text" placeholder='0' value={result} id="answer" className='button'/>
+     <input type="button" value="9" className='button' onClick={handleClick}/>
+     <input type="button" value="8" className='button' onClick={handleClick}/>
+     <input type="button" value="7" className='button' onClick={handleClick}/>
+     <input type="button" value="6" className='button' onClick={handleClick}/>
+     <input type="button" value="5" className='button' onClick={handleClick}/>
+     <input type="button" value="4" className='button' onClick={handleClick}/>
+     <input type="button" value="3" className='button' onClick={handleClick}/>
+     <input type="button" value="2" className='button' onClick={handleClick}/>
+     <input type="button" value="1" className='button' onClick={handleClick}/>
+     <input type="button" value="0" className='button' onClick={handleClick}/>
+     <input type="button" value="+" className='button' onClick={handleClick}/>
+     <input type="button" value="-" className='button' onClick={handleClick}/>
+     <input type="button" value="*" className='button' onClick={handleClick}/>
+     <input type="button" value="/" className='button' onClick={handleClick}/>
+     <input type="button" value="%" className='button' onClick={handleClick}/>
+     <input type="button" value="=" className='button button1' onClick={calculate}/>
+     <input type="button" value="Clear" className='button button1' onClick={handleClear}/>
+
     </div>
   );
 }
