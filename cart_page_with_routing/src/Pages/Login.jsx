5a07f2be23df7916d139ef03d2726fe8a89c69c4
Mohammad Saif <saifsami321md@gmail.com>
2022-11-11 22:26:35 +0530

    login_part logic is done only css is left

--- cart_page_with_routing/src/Pages/Login.jsx
+++ cart_page_with_routing/src/Pages/Login.jsx
@@ -1,10 +1,57 @@
 
-import React from "react"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+
+    // email: "eve.holt@reqres.in",
+  // password: "pistol",
+  const navigate = useNavigate()
+    const [inputdata,setInputData] = useState({
+        email:"",password:""
+    })
+
+    const FetchFunction = ({email,password}) => {
+        fetch("https://reqres.in/api/login",{
+            method:"POST",
+            headers:{"content-type":"application/json"},
+            body:JSON.stringify({
+                email:email,
+                password:password,
+            })
+        }).then((res)=>res.json()).then((data)=>{
+            console.log(data.token)
+            if(data.token){
+                navigate("/cart")
+            }
+        })
+    }
+
+    const handleChange = (e) => {
+        setInputData({
+            ...inputdata,
+            [e.target.name]:e.target.value
+        })
+    }
+    const handleFormSubmit = (e) => {
+        
+        console.log(e,"sssss")
+       e.preventDefault()
+       FetchFunction(inputdata)
+    }
     return(
         <div>
         <h1>Login</h1>
+        <form onSubmit={handleFormSubmit}>
+            <label>Email</label>
+            <input type="email" name="email" value={inputdata.email} onChange={handleChange} />
+
+            <label >Password</label>
+            <input type="password" name="password" value={inputdata.password} onChange={handleChange}/>
+
+            <button>Login</button>
+        </form>
     </div>
     )
 }
