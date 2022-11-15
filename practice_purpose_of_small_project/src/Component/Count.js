5ab73671c3c7815052dcd942b370cdc3981103ab
Mohammad Saif <saifsami321md@gmail.com>
2022-11-15 23:56:24 +0530

    counter practice

---
+++ practice_purpose_of_small_project/src/Component/Count.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useReducer } from "react";
+
+
+const initialState =0;
+
+const reducer = (state,action) => {
+    console.log(state,action)
+     if(action.type==="Increament"){
+        return state+1;
+     }
+     else if(action.type==="Decrement"){
+        return state-1;
+     }
+    }
+
+//    switch(action.type){
+//     case "Increament":
+//         return (state+=1)
+//     case "Decreament":
+//         let newcount= 0
+//         state>=1?(newcount=state-1):(newcount=0)
+//         return newcount
+//     default:
+//          return state
+//    }
+
+// }
+
+const Count = () => {
+    const [state,dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div>
+            <p>{state}</p>
+            <button onClick={()=>dispatch({type:"Increament"})}>+</button>
+            <button onClick={()=>dispatch({type:"Decreament"})}>-</button>
+        </div>
+    )
+}
+
+export default Count;
