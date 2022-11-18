100c50b5952680f61e31421fd12b971aebc70c01
Mohammad Saif <saifsami321md@gmail.com>
2022-11-18 23:55:59 +0530

    useReducer practice

--- practice_purpose_of_small_project/src/Component/Count.js
+++ practice_purpose_of_small_project/src/Component/Count.js
@@ -2,41 +2,34 @@ import React from "react";
 import { useReducer } from "react";
 
 
-const initialState =0;
-
 const reducer = (state,action) => {
-    console.log(state,action)
-     if(action.type==="Increament"){
-        return state+1;
-     }
-     else if(action.type==="Decrement"){
-        return state-1;
-     }
+    switch(action.type){
+        case "Increament":{
+            return state+1
+        }
+        case "Decreament":{
+            return state-1
+        }
+        default:
+            return state
     }
 
-//    switch(action.type){
-//     case "Increament":
-//         return (state+=1)
-//     case "Decreament":
-//         let newcount= 0
-//         state>=1?(newcount=state-1):(newcount=0)
-//         return newcount
-//     default:
-//          return state
-//    }
+}
+const initialState = 0;
 
-// }
 
 const Count = () => {
-    const [state,dispatch] = useReducer(reducer, initialState);
+
+const [state, dispatch] = useReducer(reducer,initialState)
+
 
     return (
-        <div>
-            <p>{state}</p>
-            <button onClick={()=>dispatch({type:"Increament"})}>+</button>
-            <button onClick={()=>dispatch({type:"Decreament"})}>-</button>
+        <div className="count">
+        <button onClick={()=>dispatch({type:"Increament"})}>+</button>
+                      {state}
+        <button onClick={()=>dispatch({type:"Decreament"})}>-</button>
         </div>
     )
 }
 
-export default Count;
+export default Count
--- practice_purpose_of_small_project/src/index.js
+++ practice_purpose_of_small_project/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
+// import App from './App';
+import count from "./App.css"
+import Count from './Component/Count';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <App />
+    // <App />
+    <Count/>
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
