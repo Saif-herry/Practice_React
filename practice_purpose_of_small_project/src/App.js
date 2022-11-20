9770e73bc7110d1129ba67f18bb44ac2df8e6216
Mohammad Saif <saifsami321md@gmail.com>
2022-11-20 17:23:05 +0530

    practice of useRef concept

--- practice_purpose_of_small_project/src/App.js
+++ practice_purpose_of_small_project/src/App.js
@@ -1,20 +1,21 @@
-import React, { useRef } from "react";
+import React, { useRef } from "react"
 import "./App.css"
 
 const App = () =>{
-  const countRef = useRef(0);
 
-  const Handle = () => {
-    countRef.current++;
-    console.log(`Clicked ${countRef.current} times`)
-  };
-   console.log("I rendered!")
-    return (
-      <div className="App">
-        <button onClick={Handle}>Click Me!</button>
-      </div>
-    )
+const countRef = useRef(0)
+
+const Handle = () => {
+  countRef.current++
+  console.log(`Clicked ${countRef.current} times`)
+}
+console.log("I rendered!")
+
+  return (
+    <div className="App">
+      <button onClick={Handle}>+</button>
+    </div>
+  )
 }
 
-
-export default App;
+export default App
--- practice_purpose_of_small_project/src/index.js
+++ practice_purpose_of_small_project/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import count from "./App.css"
 import Count from './Component/Count';
 
@@ -9,8 +9,8 @@ import Count from './Component/Count';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    // <App />
-    <Count/>
+    <App />
+    // <Count/>
   
 );
 
