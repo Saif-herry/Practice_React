a7ef780959083ed1429055a73f291cae7a156371
Mohammad Saif <saifsami321md@gmail.com>
2023-01-13 23:08:12 +0530

    updated

--- practice_purpose_of_small_project/src/Profile.jsx
+++ practice_purpose_of_small_project/src/Profile.jsx
@@ -1,43 +1,49 @@
 import React from 'react'
-import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+  Button,
+} from '@chakra-ui/react'
 
 const Profile = () => {
-
   return (
     <div>
-<Card maxW='-moz-fit-content'>
-  <CardBody mr={"40"}>
-    <Image
-      src='https://www.bigbasket.com/media/uploads/p/s/10000150_19-fresho-onion.jpg'
-      alt='Green double couch with wooden legs'
-      borderRadius='md'
-    />
-    <Stack mr={"-moz-initial"} mt='6' spacing='0'>
-      {/* <Heading size='md'>Living room Sofa</Heading> */}
-      <Text>Fresho</Text>
-      <Text>Onion (Loose)</Text>
-      {/* <Text>
-        This sofa is perfect for modern tropical spaces, baroque inspired
-        spaces, earthy toned spaces and for people who love a chic design with a
-        sprinkle of vintage design.
-      </Text> */}
-      <Text  color='blue.600' fontSize='2xl'>
-        $450
-      </Text>
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter>
-    <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-        Buy now
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
-        Add to cart
-      </Button>
-    </ButtonGroup>
-  </CardFooter>
-</Card>
+      <Card maxW="-moz-fit-content">
+        <CardBody mr={'40'}>
+          <Image
+            src="https://www.bigbasket.com/media/uploads/p/s/10000150_19-fresho-onion.jpg"
+            alt="Green double couch with wooden legs"
+            borderRadius="md"
+          />
+          <Stack mr={'-moz-initial'} mt="6" spacing="0">
+            <Text>Fresho</Text>
+            <Text>Onion (Loose)</Text>
+
+            <Text color="blue.600" fontSize="2xl">
+              $450
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Buy now
+            </Button>
+            <Button variant="ghost" colorScheme="blue">
+              Add to cart
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
