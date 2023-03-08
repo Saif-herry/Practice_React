48765aba77d58a3a974b69131b21db1d343b847c
Mohammad Saif <saifsami321md@gmail.com>
2023-03-08 23:14:42 +0530

    update

--- crud_operation_with_axios/src/Form.jsx
+++ crud_operation_with_axios/src/Form.jsx
@@ -1,28 +1,35 @@
 import React, { useState } from 'react'
 
-const Form = () =>{
-const [inputdata,setInputdata] = useState("")
-const [emaildata,setEmaildata] = useState("")
+const Form = () => {
+  const [inputdata, setInputdata] = useState('')
+  const [emaildata, setEmaildata] = useState('')
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
-    let email= emaildata;
-    let name = inputdata;
+    let email = emaildata
+    let name = inputdata
     let obj = {
       email,
-      name
+      name,
     }
-    console.log("InputForm:-",obj)
-
+    console.log('InputForm:-', obj)
   }
 
-  return(
+  return (
     <>
-    <form onSubmit={handleSubmit}>
-      <input placeholder='Enter Email' type="email" onChange={(e)=>setEmaildata(e.target.value)}/>
-      <input placeholder="Enter User Name" type="text" onChange={(e)=>setInputdata(e.target.value)}/>
-      <button>Submit</button>
-    </form >
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter Email"
+          type="email"
+          onChange={(e) => setEmaildata(e.target.value)}
+        />
+        <input
+          placeholder="Enter User Name"
+          type="text"
+          onChange={(e) => setInputdata(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
     </>
   )
 }
